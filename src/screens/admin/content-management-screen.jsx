4c0ed15e5692bd87cc74ed2ba2a6f07ebfa08c8c
mohaiminur24 <mohaiminur24@gmail.com>
2024-12-13 02:41:39 +0600

    edit lesson modal design is complete

--- src/screens/admin/content-management-screen.jsx
+++ src/screens/admin/content-management-screen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SectionWrapper from "../../components/common/section-wrapper";
 import { useNavigate } from "react-router-dom";
 import Modal from "../../components/common/modal";
@@ -6,14 +6,16 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useCreateLesson, useGetLessons } from "../../react-query/lessons";
 import Swal from "sweetalert2";
+import Loading from "../../components/common/loading";
 
 export default function ContentManagementScreen() {
   const navigation = useNavigate();
+  const lessons = useGetLessons();
   const [currentPage, setCurrentPage] = useState(1);
   const lessonsPerPage = 5;
   const startIndex = (currentPage - 1) * lessonsPerPage;
-  const currentLessons = lessons.slice(startIndex, startIndex + lessonsPerPage);
-  const totalPages = Math.ceil(lessons.length / lessonsPerPage);
+  const [currentLessons, setCurrentLessons] = useState([]);
+  const [totalPages, setTotalPage] = useState(0);
   const handlePrevious = () => {
     if (currentPage > 1) setCurrentPage(currentPage - 1);
   };
@@ -22,7 +24,8 @@ export default function ContentManagementScreen() {
   };
   const [isOpen, setIsOpen] = useState(false);
   const createLessons = useCreateLesson();
-  const lessons = useGetLessons();
+
+  const [isOpenUpdateModal, setOpenUpdateModal] = useState(false);
 
   const formik = useFormik({
     initialValues: {
@@ -39,32 +42,77 @@ export default function ContentManagementScreen() {
     }),
     onSubmit: async (values, { resetForm }) => {
       const res = await createLessons.mutateAsync(values);
-      if(res.success){
+      if (res.success) {
         Swal.fire({
           position: "top-end",
           icon: "success",
           title: "Your lessons has been saved",
           showConfirmButton: false,
-          timer: 1500
+          timer: 1500,
         });
-      }else{
+      } else {
         Swal.fire({
           position: "top-end",
           icon: "success",
           title: "something went wrong!",
           showConfirmButton: false,
-          timer: 1500
+          timer: 1500,
         });
       }
-      
+
       setIsOpen(false);
       resetForm();
     },
   });
 
+  const UpdateFormik = useFormik({
+    initialValues: {
+      id:null,
+      title: "",
+      description: "",
+    },
+    validationSchema: Yup.object({
+      title: Yup.string()
+        .required("Title is required")
+        .min(3, "Title must be at least 3 characters"),
+      description: Yup.string()
+        .required("Description is required")
+        .min(10, "Description must be at least 10 characters"),
+    }),
+    onSubmit: async (values, { resetForm }) => {
+      const res = await createLessons.mutateAsync(values);
+      if (res.success) {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Your lessons has been updated successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      } else {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "something went wrong!",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
 
-  console.log(lessons);
-  
+      setIsOpen(false);
+      resetForm();
+    },
+  });
+
+  useEffect(() => {
+    if (!lessons.data) return;
+    const res = lessons.data.slice(startIndex, startIndex + lessonsPerPage);
+    setCurrentLessons(res);
+    const page = Math.ceil(lessons?.data.length / lessonsPerPage);
+    setCurrentPage(page);
+  }, [lessons.isLoading]);
+
+  if (lessons.isLoading) return <Loading />;
 
   return (
     <div>
@@ -99,13 +147,16 @@ export default function ContentManagementScreen() {
                   </tr>
                 </thead>
                 <tbody>
-                  {currentLessons.map((x) => {
+                  {currentLessons.map((x, index) => {
                     return (
-                      <tr className="text-gray-600 font-extralight text-xs">
-                        <th>{x.id}</th>
+                      <tr
+                        className="text-gray-600 font-extralight text-xs"
+                        key={index}
+                      >
+                        <th>{x._id}</th>
                         <td>{x.title}</td>
                         <td>{x.description}</td>
-                        <td>{x.vocabularyCount}</td>
+                        <td>{x.count}</td>
                         <td className="flex justify-center items-center space-x-2">
                           {/* Show Vocabulary */}
                           <button
@@ -117,7 +168,14 @@ export default function ContentManagementScreen() {
                           {/* Edit Button */}
                           <button
                             className="btn btn-sm btn-accent"
-                            onClick={() => {}}
+                            onClick={() => {
+                              UpdateFormik.setFieldValue("title", x.title)
+                              UpdateFormik.setFieldValue("description", x.title)
+                              UpdateFormik.setFieldValue("id", x._id)
+                              setTimeout(() => {
+                                setOpenUpdateModal(true);
+                              }, 500);
+                            }}
                           >
                             Edit
                           </button>
@@ -136,32 +194,38 @@ export default function ContentManagementScreen() {
                 </tbody>
               </table>
             </div>
-            {/* Pagination Controls */}
-            <div className="flex justify-center mt-6 space-x-4">
-              <button
-                onClick={handlePrevious}
-                disabled={currentPage === 1}
-                className={`btn ${
-                  currentPage === 1 ? "btn-disabled" : "btn-primary btn-outline"
-                }`}
-              >
-                Previous
-              </button>
-              <span className="text-gray-700">
-                Page {currentPage} of {totalPages}
-              </span>
-              <button
-                onClick={handleNext}
-                disabled={currentPage === totalPages}
-                className={`btn ${
-                  currentPage === totalPages
-                    ? "btn-disabled"
-                    : "btn-primary btn-outline"
-                }`}
-              >
-                Next
-              </button>
-            </div>
+            {currentLessons > lessonsPerPage && (
+              <>
+                {/* Pagination Controls */}
+                <div className="flex justify-center mt-6 space-x-4">
+                  <button
+                    onClick={handlePrevious}
+                    disabled={currentPage === 1}
+                    className={`btn ${
+                      currentPage === 1
+                        ? "btn-disabled"
+                        : "btn-primary btn-outline"
+                    }`}
+                  >
+                    Previous
+                  </button>
+                  <span className="text-gray-700">
+                    Page {currentPage} of {totalPages}
+                  </span>
+                  <button
+                    onClick={handleNext}
+                    disabled={currentPage === totalPages}
+                    className={`btn ${
+                      currentPage === totalPages
+                        ? "btn-disabled"
+                        : "btn-primary btn-outline"
+                    }`}
+                  >
+                    Next
+                  </button>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </SectionWrapper>
@@ -240,52 +304,84 @@ export default function ContentManagementScreen() {
           </div>
         </form>
       </Modal>
+      <Modal
+        isOpen={isOpenUpdateModal}
+        onClose={() => setOpenUpdateModal(false)}
+      >
+        <form onSubmit={UpdateFormik.handleSubmit} className="p-6">
+          {/* Title Field */}
+          <div className="mb-4">
+            <label
+              htmlFor="title"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              Title
+            </label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={UpdateFormik.values.title}
+              onChange={UpdateFormik.handleChange}
+              onBlur={UpdateFormik.handleBlur}
+              className={`w-full px-4 py-2 border rounded-md bg-transparent text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                UpdateFormik.touched.title && UpdateFormik.errors.title
+                  ? "border-red-500"
+                  : "border-gray-300"
+              }`}
+            />
+            {UpdateFormik.touched.title && UpdateFormik.errors.title && (
+              <p className="text-red-500 text-sm mt-1">
+                {UpdateFormik.errors.title}
+              </p>
+            )}
+          </div>
+          {/* Description Field */}
+          <div className="mb-4">
+            <label
+              htmlFor="description"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              Description
+            </label>
+            <textarea
+              id="description"
+              name="description"
+              value={UpdateFormik.values.description}
+              onChange={UpdateFormik.handleChange}
+              onBlur={UpdateFormik.handleBlur}
+              className={`w-full px-4 py-2 border rounded-md bg-transparent text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                UpdateFormik.touched.description &&
+                UpdateFormik.errors.description
+                  ? "border-red-500"
+                  : "border-gray-300"
+              }`}
+            ></textarea>
+            {UpdateFormik.touched.description &&
+              UpdateFormik.errors.description && (
+                <p className="text-red-500 text-sm mt-1">
+                  {UpdateFormik.errors.description}
+                </p>
+              )}
+          </div>
+          {/* Submit Button */}
+          <div className="flex justify-end">
+            <button
+              type="button"
+              onClick={() => setOpenUpdateModal(false)}
+              className="mr-4 bg-gray-300 text-gray-800 py-2 px-4 rounded-md font-semibold hover:bg-gray-400 focus:outline-none"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="bg-blue-600 text-white py-2 px-4 rounded-md font-semibold hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+            >
+              update
+            </button>
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 }
-
-const lessons = [
-  {
-    id: 1,
-    title: "Basic Greetings",
-    description:
-      "Learn essential Japanese greetings for everyday conversations.",
-    vocabularyCount: 10,
-  },
-  {
-    id: 2,
-    title: "Numbers and Counting",
-    description: "Master the numbers and counting system in Japanese.",
-    vocabularyCount: 15,
-  },
-  {
-    id: 3,
-    title: "Family Members",
-    description: "Understand how to refer to family members in Japanese.",
-    vocabularyCount: 12,
-  },
-  {
-    id: 4,
-    title: "Food and Drinks",
-    description: "Learn Japanese vocabulary for common foods and drinks.",
-    vocabularyCount: 20,
-  },
-  {
-    id: 5,
-    title: "Shopping Phrases",
-    description: "Useful phrases and vocabulary for shopping in Japan.",
-    vocabularyCount: 18,
-  },
-  {
-    id: 6,
-    title: "Travel Essentials",
-    description: "Important vocabulary for traveling in Japan.",
-    vocabularyCount: 22,
-  },
-  {
-    id: 7,
-    title: "Workplace Terms",
-    description: "Learn workplace-related vocabulary in Japanese.",
-    vocabularyCount: 17,
-  },
-];
